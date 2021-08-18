65f64d7c5e285506be3cb124d0c027692a1d3ebe
DimasVeliz <trevor90094@yandex.com>
2021-08-18 15:10:23 +0200

    For and while loops samples added

--- Topics/Loops/For_Loop.cs
+++ Topics/Loops/For_Loop.cs
@@ -4,5 +4,98 @@ namespace Loops
 {
     public class For_Loop
     {
+        //returns the position of the item if found, else returns -1
+        public static int LinearSearch(int[] numbers,int item)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if(numbers[i]==item)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        //return the max number of the array
+        public static int FindMaxNumber(int[] numbers)
+        {
+            if (numbers.Length==0)
+            {
+                throw new ArgumentException("An empty array should not be passed");
+            }
+
+            int maxTemporal=int.MinValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (maxTemporal> numbers[i])
+                {
+                    maxTemporal=numbers[i];
+                }
+            }
+            return maxTemporal;
+        }
+
+
+        //returns the result of multiplying a and b without using built-in function
+        public static int Multiplication(int a, int b)
+        {
+            int result =0;
+
+            for (int i = 0; i < b; i++)
+            {
+                result+=a;
+            }
+
+            return result;
+        }
+        
+
+        public static float Average(int[] numbers)
+        {
+            if (numbers.Length==0)
+            {
+                throw new ArgumentException("An empty array should not be passed");
+            }
+            
+            int sum=0;
+            for (int i = 0; i < length; i++)
+            {
+                sum+=numbers[i];
+            }
+
+            return sum/numbers.Length;
+        }
+
+        public static int LinearPower(int a, int b)
+        {
+            int result =1;
+
+            for (int i = 0; i < b; i++)
+            {
+                result*=a;
+            }
+
+            return result;
+        }
+
+        public static int Factorial(int a)
+        {
+            if (a==0 || a==1)
+            {
+                return 1;
+            }
+
+            int result =1;
+
+
+            for (int i = 2; i <= a; i++)
+            {
+                result+=i;
+            }
+
+            return result;
+        }
     }
 }
--- Topics/Loops/While_Loop.cs
+++ Topics/Loops/While_Loop.cs
@@ -4,5 +4,63 @@ namespace Loops
 {
     public class While_Loop
     {
+        public static int CountPositiveNumbers(int[] numbers)
+        {
+            int index = 0;
+            int counter = 0;
+            while (index < numbers.Length)
+            {
+                if (numbers[index] > 0)
+                {
+                    counter++;
+                }
+
+                index++;
+            }
+
+            return counter;
+        }
+
+        public static int BinarySearch(int[] numbers, int item)
+        {
+            int min = 0;
+            int max = numbers.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                //checking whether I've found the number
+                if (item == numbers[mid])
+                {
+                    return ++mid;
+                }
+
+                //given that I didnt found it, it should be at the left or right.
+                else if (item < numbers[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
+            //not found
+            return -1;
+        }
+
+        public static int GCD(int a, int b)
+        {
+            int r=a%b;
+
+            while (r!=0)
+            {
+                r=a%b;
+                a=b;
+                b=r;
+            }
+            return a;
+            
+        }
     }
 }
